c5ecca75e7db5b6c573488eaa5afb3362352a6db
aaronlbrink <aaron@brinkdatabase.com>
2021-11-13 11:23:22 -0800

    Bootstrap backend

---
+++ packages/api/config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.appId = "<your-app-id-here>";
+config.hashToken = "<your-hash-token-here>";
+config.authTokenUrl = "https://api.iq.inrix.com/auth/v1/appToken";
+module.exports = config;
