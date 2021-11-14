423922c776db45acba597db0794258fca4642d20
Asad Baloch <45577524+abaloch@users.noreply.github.com>
2021-11-13 18:15:17 -0800

    Add cors

--- packages/api/asad.js
+++ packages/api/asad.js
@@ -1,6 +1,14 @@
 import express, { application } from 'express';
 import fetch from 'node-fetch';
 let app = express()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+
 app.get("/hi", (req, res) => {
     res.json({hello: "hi"});
 })
